c9bcfdc6774b453321f04c8f110c592cf39b9d4b
ductrung1401 <tranductrung12e01@gmail.com>
2024-03-01 17:37:23 +0700

    microservice basic

---
+++ employee-service2/src/main/java/com/trungtd/employeeservice/response/MessageResponse.java
@@ -0,0 +1,11 @@
+package com.trungtd.employeeservice.response;
+
+import lombok.*;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
